ecb1d3aaeec54d2668d63cba0587250a1edcfc00
Atamoghlan <atamoghlan.a.aliyev@gmail.com>
2020-10-01 12:44:45 +0400

    FinalVersion

--- Dictionary/Dictionary/Program.cs
+++ Dictionary/Dictionary/Program.cs
@@ -8,12 +8,16 @@ namespace Dictionary
         public static void Main()
         {
             string answer = "";
-            
+            int RemoveOrNot = 0;
+            int remove = 0;
+            int S_count = 0;
+            int A_count = 0;
             Dictionary<int, Student> AllStudents = new Dictionary<int, Student>();
             Dictionary <int, Aspirant> AllAspirants = new Dictionary<int, Aspirant>();
         Link1:
             Console.WriteLine("Ввести студента - 1\nВвести аспиранта - 2\nСписок студентов - 3\nСписок аспирантов -4");
             answer = Console.ReadLine();
+         
             switch (answer)
             {
                 case ("1"):
@@ -38,8 +42,10 @@ namespace Dictionary
                 int course = 0;
                 int creditbooknumber = 0;
                 int dissertation = 0;
+                
                 while (answer == "1")
                 {
+                    
                     while (lastname == null || lastname.Length < 2 || lastname.Length > 30)
                     {
                         Console.WriteLine("Введите фамилию студента");
@@ -57,6 +63,7 @@ namespace Dictionary
                             continue;
                         }
                         else { Console.WriteLine("Ошибка!"); }
+                        
                     }
                     while (course < 1 || course > 5)
                     {
@@ -83,7 +90,7 @@ namespace Dictionary
                         {
                             creditbooknumber = Convert.ToInt32(Console.ReadLine());
                         }
-                        catch (Exception)
+                        catch 
                         {
                             Console.WriteLine("Введите цифру от 1 до 10000(включительно)");
                             continue;
@@ -94,16 +101,15 @@ namespace Dictionary
                         }
                         else { Console.WriteLine("Ошибка!"); }
                     }
-                    Student newStudent = new Student(lastname, course, creditbooknumber);
-                    int count = 1;
-                    AllStudents.Add(count, newStudent);
-                    count += 1;
+                    S_count += 1;
+                    AllStudents.Add(S_count, new Student(lastname, course, creditbooknumber));
+                    break;   
                 }
                 while(answer =="2")
                 {
                     while (lastname == null || lastname.Length < 2 || lastname.Length > 30)
                     {
-                        Console.WriteLine("Введите фамилию студента");
+                        Console.WriteLine("Введите фамилию аспиранта");
                         try
                         {
                             lastname = Console.ReadLine();
@@ -121,7 +127,7 @@ namespace Dictionary
                     }
                     while (course < 1 || course > 5)
                     {
-                        Console.WriteLine("Введите курс студента");
+                        Console.WriteLine("Введите курс аспиранта");
                         try
                         {
                             course = Convert.ToInt32(Console.ReadLine());
@@ -166,9 +172,9 @@ namespace Dictionary
                         else { Console.WriteLine("Ошибка!"); }
                     }
                     Aspirant newAspirant = new Aspirant(lastname, course, creditbooknumber,dissertation);
-                    int count = 1;
-                    AllAspirants.Add(count, newAspirant);
-                    count += 1;
+                    A_count += 1;
+                    AllAspirants.Add(A_count, newAspirant);
+                    break;
                 }
                 while (answer == "3")
                 {
@@ -177,6 +183,52 @@ namespace Dictionary
                     {
                         a.Print();
                     }
+                    while (RemoveOrNot != 1 && RemoveOrNot != 2)
+                    {
+                        Console.WriteLine("Хотите удалить студента из списка?\n1 - Да \t 2 - Нет");
+                        try
+                        {
+                            RemoveOrNot = Convert.ToInt32(Console.ReadLine());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Введите либо 1, либо 2");
+                            continue;
+                        }
+                        if (RemoveOrNot ==1 || RemoveOrNot ==2)
+                        {
+                            while (RemoveOrNot == 1)
+                            {
+                                Console.WriteLine("Введите какого по счету студента хотите удалить из списка (пример: 1 - удалить первого, 2 - удалить второго т.д.");
+                                try
+                                {
+                                    remove = Convert.ToInt32(Console.ReadLine());
+                                }
+                                catch (Exception)
+                                {
+                                    Console.WriteLine("Ошибка! Введите цифру в соответствии от списка");
+                                    continue;
+                                }
+                                foreach (KeyValuePair<int, Student> g in AllStudents)
+                                {
+                                    if (remove == g.Key)
+                                    {
+                                        AllStudents.Remove(remove);
+                                        Console.WriteLine("Студент под индексом {remove} успешно удален\n");
+                                        break;
+                                    }
+                                    else
+                                    { Console.WriteLine("Ошибка!"); }
+                                    continue;
+                                }
+                                break;
+                            }
+                           
+                        }
+                        else { Console.WriteLine("Ошибка!Введите либо 1, либо 2"); }
+
+                    }
+                    break;
                 }
                 while (answer == "4")
                 {
@@ -184,6 +236,52 @@ namespace Dictionary
                     {
                         b.Print();
                     }
+                    while (RemoveOrNot != 1 && RemoveOrNot != 2)
+                    {
+                        Console.WriteLine("Хотите удалить студента из списка?\n1 - Да \t 2 - Нет");
+                        try
+                        {
+                            RemoveOrNot = Convert.ToInt32(Console.ReadLine());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Введите либо 1, либо 2");
+                            continue;
+                        }
+                        if (RemoveOrNot == 1 || RemoveOrNot == 2)
+                        {
+                            while (RemoveOrNot == 1)
+                            {
+                                Console.WriteLine("Введите какого по счету студента хотите удалить из списка (пример: 1 - удалить первого, 2 - удалить второго т.д.");
+                                try
+                                {
+                                    remove = Convert.ToInt32(Console.ReadLine());
+                                }
+                                catch (Exception)
+                                {
+                                    Console.WriteLine("Ошибка! Введите цифру в соответствии от списка");
+                                    continue;
+                                }
+                                foreach (KeyValuePair<int, Student> g in AllStudents)
+                                {
+                                    if (remove == g.Key)
+                                    {
+                                        AllStudents.Remove(remove);
+                                        Console.WriteLine("Студент под индексом {remove} успешно удален\n");
+                                        break;
+                                    }
+                                    else
+                                    { Console.WriteLine("Ошибка!"); }
+                                    continue;
+                                }
+                                break;
+                            }
+
+                        }
+                        else { Console.WriteLine("Ошибка!Введите либо 1, либо 2"); }
+
+                    }
+                    break;
                 }
             }
         }
